35dbe5014ad9b4740384ffa4311d6f89e782dbaf
Anastasiia <nsydorchenko@gmail.com>
2021-02-23 19:43:31 +0200

    add header

--- app/js/app.js
+++ app/js/app.js
@@ -11,3 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 
 })
+
+// HAMBURGER MENU 
+$(document).ready(function () {
+   $('.header__burger').click(function (event) {
+      $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock'); // remove scroll when burger menu is active
+   });
+
+   $('.header__menu').click(function () {
+      $('.header__burger, .header__menu').removeClass('active');
+      $('body').removeClass('lock'); // close burger menu after click
+   });
+});
