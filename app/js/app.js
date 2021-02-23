7a891d7340d97da94c6050bd28b9eec66a50c093
Anastasiia <nsydorchenko@gmail.com>
2021-02-23 23:20:42 +0200

    add burger menu

--- app/js/app.js
+++ app/js/app.js
@@ -3,24 +3,11 @@
 // window.jQuery = $
 // window.$ = $
 
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+// Import vendor jQuery plugin example (not module)
+//require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
 })
-
-// HAMBURGER MENU 
-$(document).ready(function () {
-   $('.header__burger').click(function (event) {
-      $('.header__burger, .header__menu').toggleClass('active');
-      $('body').toggleClass('lock'); // remove scroll when burger menu is active
-   });
-
-   $('.header__menu').click(function () {
-      $('.header__burger, .header__menu').removeClass('active');
-      $('body').removeClass('lock'); // close burger menu after click
-   });
-});
