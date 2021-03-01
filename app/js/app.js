ada65d83e7ab053235f10809b4e3e092f9e65060
Anastasiia <nsydorchenko@gmail.com>
2021-03-01 18:41:50 +0200

    add footer & dropdown menu

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,8 @@
 
 // import Swiper JS
 import Swiper from 'swiper';
+import popper from '@popperjs/core';
+import bootstrap from 'bootstrap';
 
 // Import vendor jQuery plugin example (not module)
 //require('~/app/libs/mmenu/dist/mmenu.js')
